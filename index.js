9b18de901a6d22972533bdbb832282bb0e435556
active-researcher-7876 <elliott.daigle@gmail.com>
2019-07-19 20:29:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   else if (string === uppercase) {
     return "YES INDEED!"
   }
-  else if (string === "I love you, Grandma.") {
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
